5d957189e3e0d3c73cc4ae0785f4c1d7720e93a8
KMaybe <45684830+KMaybe01@users.noreply.github.com>
2024-04-08 22:59:44 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-    base: "/resume/",
     bundler: viteBundler(),
     theme: defaultTheme({
         search:false,
