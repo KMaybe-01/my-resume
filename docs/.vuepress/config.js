a02412262df42d1c8d32d5f9d1952f6895d35f50
Weijie Chen <chenweijie@casachina.com.cn>
2024-04-08 17:49:49 +0800

    init my resume

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        search:false,
+        navbar:false,
+    }),
+})
