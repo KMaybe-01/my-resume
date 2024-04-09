a417f4cbd66a7932019e05c0fe87f05ac9af13d1
Weijie Chen <chenweijie@casachina.com.cn>
2024-04-09 13:24:13 +0800

    Deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default';
 import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
-    base: '/resume',
+    base: '/resume/docs',
     title: 'Resume',
     description: 'Resume',
 
